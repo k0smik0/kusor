9fa81b4173cdae8f619f6c25f30bd7efc9819745
Massimiliano Leone <massimiliano.leone@iubris.net>
2012-07-12 00:18:57 +0200

    fixed license stuffs

--- src/main/net/iubris/kusor/utils/LocationUtils.java
+++ src/main/net/iubris/kusor/utils/LocationUtils.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
  * Copyleft 2012 Massimiliano Leone - massimiliano.leone@iubris.net .
  * 
- * LocationUtils.java is part of kusor.
+ * LocationUtils.java is part of 'Kusor'.
  * 
- * kusor is free software; you can redistribute it and/or modify
+ * 'Kusor' is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  * 
- * kusor is distributed in the hope that it will be useful,
+ * 'Kusor' is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with kusor ; if not, write to the Free Software
+ * along with 'Kusor' ; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  ******************************************************************************/
 package net.iubris.kusor.utils;
