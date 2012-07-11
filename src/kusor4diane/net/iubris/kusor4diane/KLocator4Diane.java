9a89d031e97d4b4980f248685ab6d523894544a4
Massimiliano Leone <massimiliano.leone@iubris.net>
2012-07-12 00:19:15 +0200

    added kusor4diane

---
+++ src/kusor4diane/net/iubris/kusor4diane/KLocator4Diane.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyleft 2012 Massimiliano Leone - massimiliano.leone@iubris.net .
+ * 
+ * KLocator4Diane.java is part of 'Kusor'.
+ * 
+ * 'Kusor' is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * 'Kusor' is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with 'Kusor' ; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ ******************************************************************************/
+package net.iubris.kusor4diane;
+
+
+import net.iubris.diane.searcher.locationaware.locator.Locator;
+import net.iubris.diane.searcher.locationaware.observatory.action.LocationNotificationAction;
+import net.iubris.diane.searcher.locationaware.observatory.observable.KlocatorObservableSyncObservableDelegate;
+import net.iubris.diane.searcher.locationaware.observatory.observer.LocationObserver;
+import net.iubris.kusor.locator.KLocator;
+import android.app.Application;
+import android.location.Location;
+
+import com.google.inject.Inject;
+import com.novoda.location.LocatorSettings;
+
+public class KLocator4Diane extends KLocator implements Locator {
+
+	private final KlocatorObservableSyncObservableDelegate syncObservableDelegate;
+
+	@Inject
+	public KLocator4Diane(Application context, 	LocatorSettings novodaLocatorSettings,
+			KlocatorObservableSyncObservableDelegate syncObservableDelegate) {
+		super(context, novodaLocatorSettings);
+		this.syncObservableDelegate = syncObservableDelegate;
+	}
+	
+	@Override
+	public void attachObserver(LocationObserver observer) {
+		syncObservableDelegate.attachObserver(observer);
+	}
+	@Override
+	public void detachObserver(LocationObserver observer) {
+		syncObservableDelegate.detachObserver(observer);	
+	}
+	
+	@Override
+	public void notifyObserver(LocationNotificationAction action) {
+		syncObservableDelegate.notifyObserver(action);
+	}
+	
+	
+	@Override
+	protected void onNewLocation(final Location location) {
+		super.onNewLocation(location);		
+		
+		notifyObserver( new LocationNotificationAction() {
+			@Override
+			public void execute(LocationObserver observer) {
+//Ln.d("notifying location: "+location);				
+				observer.onLocationUpdate(location);
+			}
+		});		
+	}
+}
